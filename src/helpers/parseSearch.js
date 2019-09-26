6f481483ece2e3c9a660cfe04644ff3209b11227
Kessy Similien <simkessy@gmail.com>
2019-09-26 02:28:52 -0700

    Handle response parsing

--- src/helpers/parseSearch.js
+++ src/helpers/parseSearch.js
@@ -2,20 +2,23 @@ const parseType = (type, item) => {
   switch (type) {
     case "events":
       return {
+        id: item.event.id,
         title: item.event.name,
         subtitle: item.venue.name,
         image: item.performers[0].hero_image_url
       };
     case "performers":
       return {
+        id: item.id,
         title: item.name,
-        subtitle: item.name,
+        subtitle: item.category,
         image: item.hero_image_url
       };
     case "venues":
       return {
+        id: item.id,
         title: item.name,
-        subtitle: item.name,
+        subtitle: item.city,
         image: item.image_url
       };
     default:
